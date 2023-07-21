36a976b0a718ff0081a592e909cb58e3f586e776
ihorbilash <95218369+ihorbilash@users.noreply.github.com>
2023-07-21 10:47:57 +0300

    Update HelloWorld.vue
    change message

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg1 }}</h1>
+    <h1>{{ msg }}</h1>
     
   </div>
 </template>
@@ -9,7 +9,7 @@
 export default {
   name: 'HelloWorld',
   props:[
-    'msg1'
+    'msg'
 ]
 }
 </script>
