b105d6671ca05bc71a4699cd6d92f6d8fa83562f
ihorbilash <first2007.ib@gmail.com>
2023-07-26 15:50:24 +0300

    eslint

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 
-<template >
-  <div id="app">
-    <Board />
-  </div>
+<template>
+    <div id="app">
+        <Board />
+    </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
-import Board from './components/Board.vue'
+import Board from './components/Board.vue';
 export default defineComponent({
-  name: 'App',
-  components: {
-    Board,
-  },
-}
-)
+    name: 'App',
+    components: {
+        Board,
+    },
+},
+);
 </script>
 
 <style src="./assets/trello.css"></style>
