cbb43799df7885991df84c96b011284478f2462f
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-08-14 13:58:57 +0300

    rewrite base url to relevant

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -2,6 +2,6 @@
 // eslint-disable-next-line no-undef
 const BASE_URL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://esexqht4ce.execute-api.eu-central-1.amazonaws.com';
+    : 'https://vcu42752j8.execute-api.eu-central-1.amazonaws.com';
 
 export default BASE_URL;
