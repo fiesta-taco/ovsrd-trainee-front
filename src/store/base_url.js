e019b0ad1667556f7f12f1b1a419a2a39732aa76
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-08-14 20:21:53 +0300

    new url for dev uniq

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -2,6 +2,6 @@
 // eslint-disable-next-line no-undef
 const BASE_URL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://af1vg5nbrf.execute-api.eu-central-1.amazonaws.com';
+    : 'https://vs950dudb3.execute-api.eu-central-1.amazonaws.com';
 
 export default BASE_URL;
