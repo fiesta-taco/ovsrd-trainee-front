4bdc69c587e555e6dfcdc9122fb5296c48a3c73c
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-08-15 12:55:31 +0300

    new url

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -2,6 +2,6 @@
 // eslint-disable-next-line no-undef
 const BASE_URL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://tfv4i51273.execute-api.eu-central-1.amazonaws.com';
+    : 'https://em2cmidpd9.execute-api.eu-central-1.amazonaws.com';
 
 export default BASE_URL;
