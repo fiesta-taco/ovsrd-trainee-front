813e864d4a465e4fc0e915999b67264a90326a81
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-08-15 13:41:04 +0300

    fix url

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -1,9 +1,6 @@
-
-console.log("node-env ->>>>", process.env.NODE_ENV)
-
 // eslint-disable-next-line no-undef
 const BASE_URL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
     : 'https://em2cmidpd9.execute-api.eu-central-1.amazonaws.com';
-   
+
 export default BASE_URL;
