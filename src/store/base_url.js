a2e5e57e8fc33457eee50c6d855c46b4452ee087
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-08-22 16:34:02 +0300

    test https

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 const value = process.env.NODE_ENV;
 let BASE_URL = '';
-export const IMAGE_BACKEND_URL = 'https://ServiceLBE9A1ADBC-N7f7RsvupFKZ-1466839129.eu-central-1.elb.amazonaws.com';
+export const IMAGE_BACKEND_URL = 'https://servicelbe9a1adbc-n7f7rsvupfkz-1466839129.eu-central-1.elb.amazonaws.com/';
     
 if(value === 'development'){
     BASE_URL = 'http://localhost:3000/dev';
