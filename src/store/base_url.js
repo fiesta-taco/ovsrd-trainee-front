96882b3d91a8707a27925f64a43e8ee1a0b864e7
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-10-18 21:21:31 +0300

    new

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-undef
 const value = process.env.NODE_ENV;
 let BASE_URL = '';
-export const IMAGE_BACKEND_URL ='http://docker-Servi-fH6129ePRXND-783403714.eu-central-1.elb.amazonaws.com';// 'https://bilash-docker-api-dev.kozub.dev'; //'http://localhost:3003'
+export const IMAGE_BACKEND_URL ='http://docker-Servi-fH6129ePRXND-783403714.us-east-1.elb.amazonaws.com';// 'https://bilash-docker-api-dev.kozub.dev'; //'http://localhost:3003'
     
 if(value === 'development'){
     BASE_URL = 'http://localhost:3000/dev';
 }else if(value === 'main'){
-    BASE_URL = 'https://5mhmqfwuvc.execute-api.new region.amazonaws.com/main';
+    BASE_URL = 'https://5mhmqfwuvc.execute-api.us-east-1.amazonaws.com/main';
 }else{
-    BASE_URL = 'https://5mhmqfwuvc.execute-api.new region.amazonaws.com/dev';
+    BASE_URL = 'https://mgabky167b.execute-api.us-east-1.amazonaws.com/dev';
 }
 
 
