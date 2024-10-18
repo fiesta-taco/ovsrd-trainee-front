e39cfb38697e7a7b92d5088d4b4f39be450257e9
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-10-18 17:22:38 +0300

    add new endpiont to backend docker in ecs

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 const value = process.env.NODE_ENV;
 let BASE_URL = '';
-export const IMAGE_BACKEND_URL ='https://bilash-docker-api-dev.kozub.dev';// 'https://bilash-docker-api-dev.kozub.dev'; //'http://localhost:3003'
+export const IMAGE_BACKEND_URL ='http://docker-Servi-fH6129ePRXND-783403714.eu-central-1.elb.amazonaws.com';// 'https://bilash-docker-api-dev.kozub.dev'; //'http://localhost:3003'
     
 if(value === 'development'){
     BASE_URL = 'http://localhost:3000/dev';
@@ -10,7 +10,6 @@ if(value === 'development'){
 }else{
     BASE_URL = 'https://vu77hwfuh6.execute-api.eu-central-1.amazonaws.com/dev';
 }
-
 
 
 export default BASE_URL;
