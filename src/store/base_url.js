84e1ff2b3e501663623e11643afa8884dc3b6d9f
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-10-20 17:53:47 +0300

    test

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 const value = process.env.NODE_ENV;
 let BASE_URL = '';
-export const IMAGE_BACKEND_URL ='';// 'https://bilash-docker-api-dev.kozub.dev'; //'http://localhost:3003'
+export const IMAGE_BACKEND_URL ='https://docker-servi-objooddgehrm-789589824.eu-central-1.elb.amazonaws.com';// 'https://bilash-docker-api-dev.kozub.dev'; //'http://localhost:3003'
     
 if(value === 'development'){
     BASE_URL = 'http://localhost:3000/dev';
