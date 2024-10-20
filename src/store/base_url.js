2db73993e9e2f168e51db6011704d05f0bd918e7
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-10-20 21:35:51 +0300

    new endpoints

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -6,7 +6,7 @@ export const IMAGE_BACKEND_URL ='https://docker-servi-objooddgehrm-789589824.eu-
 if(value === 'development'){
     BASE_URL = 'http://localhost:3000/dev';
 }else if(value === 'main'){
-    BASE_URL = 'https://5la0oavkn6.execute-api.eu-central-1.amazonaws.com/main';
+    BASE_URL = 'https://ko489pwes9.execute-api.eu-central-1.amazonaws.com/main';
 }else{
     BASE_URL = 'https://vu77hwfuh6.execute-api.eu-central-1.amazonaws.com/dev';
 }
