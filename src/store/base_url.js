e2cf890eb0d804801586bb05fd22f6267021be23
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-11-24 03:52:24 +0200

    a

--- src/store/base_url.js
+++ src/store/base_url.js
@@ -6,7 +6,8 @@ export const IMAGE_BACKEND_URL ='https://api-main.docker.devoops.click';// 'http
 if(value === 'development'){
     BASE_URL = 'http://localhost:3000/dev';
 }else if(value === 'main'){
-    BASE_URL = 'https://s3rnqozum6.execute-api.eu-central-1.amazonaws.com/main';
+    BASE_URL = 'https://dpn6vlw36c.execute-api.eu-central-1.amazonaws.com/main';
+    
 }else{
     BASE_URL = 'https://vu77hwfuh6.execute-api.eu-central-1.amazonaws.com/dev';
 }
